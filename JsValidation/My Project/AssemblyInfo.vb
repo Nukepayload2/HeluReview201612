163cec02f39884af87433c3615ab000b4fbe0072
Nukepayload2 <1939357182@qq.com>
2016-12-29 14:09:42 +0800

    添加自定义表单验证示例

---
+++ JsValidation/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 检查程序集特性的值
+<Assembly: AssemblyTitle("JsValidation")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("JsValidation")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("afa34e73-76b2-4332-af17-875386396d54")>
+
+' 程序集的版本信息由下列四个值组成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用 "*":
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ JsValidation/validate.js
@@ -0,0 +1,33 @@
+﻿function isNullOrWhitespace(val)
+{
+    return val == undefined || val.trim().length == 0;
+}
+function validate()
+{
+    var txtName = document.getElementById("txtName");
+    var txtAge = document.getElementById("txtAge");
+    var txtMail = document.getElementById("txtMail");
+    var lstGrade = document.getElementById("lstGrade");
+    var canSubmit = false;
+    if (isNullOrWhitespace(txtName.value))
+    {
+        tblError.innerText = "姓名不得为空";
+    }
+    else if (isNaN(txtAge.value) || txtAge.value < 10 || txtAge.value > 60)
+    {
+        tblError.innerText = "年龄不得为空且必须在 10-60 之间";
+    }
+    else if (! /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/.test(txtMail.value))
+    {
+        tblError.innerText = "请输入正确的电子邮件地址";
+    }
+    else if (isNaN(lstGrade.value) || lstGrade.value < 1)
+    {
+        tblError.innerText = "请选择年级";
+    }
+    else
+    {
+        canSubmit = true;
+    }
+    return canSubmit;
+}
